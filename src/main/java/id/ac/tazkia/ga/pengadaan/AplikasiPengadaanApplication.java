34a4918a9c20444b22d9720999e6637a8efee9b3
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-10 14:53:00 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/ga/pengadaan/AplikasiPengadaanApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.ga.pengadaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiPengadaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiPengadaanApplication.class, args);
+	}
+}
