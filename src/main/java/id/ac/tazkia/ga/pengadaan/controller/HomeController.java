a4964dabdec88271365d11a7f19510f89f2b4422
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-11 13:41:32 +0700

    implement layout template

---
+++ src/main/java/id/ac/tazkia/ga/pengadaan/controller/HomeController.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.ga.pengadaan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
