e1b462d666cd32ecae7bdf985ca143f061dc1e3b
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-11 16:12:34 +0700

    starter file untuk tampilan

---
+++ src/main/java/id/ac/tazkia/ga/pengadaan/controller/KategoriController.java
@@ -0,0 +1,26 @@
+package id.ac.tazkia.ga.pengadaan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/kategori")
+public class KategoriController {
+
+    @GetMapping("/list")
+    public void list(){
+
+    }
+
+    @GetMapping("/form")
+    public void tampilkanForm(){
+
+    }
+
+    @PostMapping("/form")
+    public String prosesForm(){
+        return "redirect:kategori/list";
+    }
+}
